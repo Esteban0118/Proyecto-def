2454229acde8fe3c03a64d34c851a85193a244b4
CarcoxGG <minecarcox@gmail.com>
2021-04-26 14:25:39 -0500

    Empezamos la creacion de componentes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
-      <h2>Adios</h2>
-      <h3>Sebastian</h3>
-      <h4>Carlos</h4>
-      <h1>Prueba</h1>
-      <p>David</p>
-      <p>no fue mi culpa</p>
-      <h5>Sebastian se lo tiro</h5>
     </div>
   );
 }
