8d75c237bc357406cf11bfe07ceb5ffef16770da
SebastianC99 <sebiscastrovivas@hotmail.com>
2021-06-07 16:27:55 -0500

    inici sesion

--- src/Components/pages/IniciarSesion.js
+++ src/Components/pages/IniciarSesion.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import '../../Components/pages/IniciarSesion.css';
+import '../../App.css'
 import LoginForm from './LoginForm';
 
 export default function Temperaturas(){
-const adminUser={
-email:"admin@admin.com",
-paswoord:"admin123"
+const adminUser = {
+    email: "admin@admin.com",
+    password: "123"
 }
 
 
@@ -14,10 +15,21 @@ const [error, setError] = useState("");
 
 const Login = details =>{
     console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password){
+        console.log("Inicio correcto");
+        setUSer({
+            name: details.name, 
+            email : details.email
+        })
+    } else {
+        console.log("Inicio fallido");
+        setError("Inicio fallido"); 
+    }
 }
 
-const Logout =()=>{
-    console.Log("logout");
+const Logout = () => {
+    setUSer({name: "", email: ""});
 }
 
 return(
@@ -25,7 +37,7 @@ return(
         {(user.email !="") ? (
         <div className =  "Bienvenido" >
             <h2>Bienvenido,<span>{user.name}</span></h2>
-            <button>Salir</button>
+            <button onClick={Logout}>Salir</button>
         </div>
         ):(
             <LoginForm Login={Login} error={error}/>
