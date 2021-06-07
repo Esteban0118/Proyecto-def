dcc50f951242c1d92f7bf26378049b853b3f4f4d
SebastianC99 <sebiscastrovivas@hotmail.com>
2021-06-07 16:28:16 -0500

    inicio sesion

--- src/Components/pages/LoginForm.js
+++ src/Components/pages/LoginForm.js
@@ -11,18 +11,19 @@ const submitHandler = e =>{
     return(
         <form onSubmit={submitHandler}>
             <div className="form-inner">        
-                <h2>iniciar sesion</h2>
+                <h2>INICIAR SESION</h2>
+                {(error != "") ? (<div className="error">{error}</div>): "" }
                     <div className="form-group">
                         <label htmlfor="name">Nombre:</label>
-                        <input type="text" name="name" id="name"/>
+                        <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value })} value={details.name} />
                     </div>    
                     <div className="form-group">
                         <label htmlFor="email">Email: </label>
-                        <input type="email" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value })} value={details.name}/>
+                        <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value })} value={details.email}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Contraseña:  </label>
-                        <input type="password" name="password" id="password"/>
+                        <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value })} value={details.password} />
                     </div>
                     <input type="submit" value="iniciar" />
             </div>
