4af64125233ff6234573adf3f52c44dcacf62bb5
cdramirez04 <minecarcox@gmail.com>
2021-06-11 23:53:19 -0500

    Cambios finales

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import './App.css';
 import Home from './Components/pages/Home';
 import Temperaturas from './Components/pages/Temperaturas';
 import TemperaturasEst from './Components/pages/TemperaturasEst';
-import IniciarSesion from './Components/pages/IniciarSesion';
-import Registrarse from './Components/pages/Registrarse';
 import Formulario from './Components/pages/Formulario';
 
 function App() {
@@ -16,13 +14,8 @@ function App() {
       <Navbar/>
       <Switch>
         <Route path='/' exact component={Home} />
-        <Route path='Temperaturas' component={Temperaturas} />
-        <Route path='/iniciarsesion' component={IniciarSesion} />
-        <Route path='Registrarse' component={Registrarse} />
         <Route path='/temperaturas' component={Temperaturas} />
         <Route path='/temperaturaest' component={TemperaturasEst} />
-        <Route path='/iniciarsesion' component={IniciarSesion} />
-        <Route path='/registrarse' component={Registrarse} />
         <Route path='/Formulario' component ={Formulario}/>
       </Switch>
     </Router>
